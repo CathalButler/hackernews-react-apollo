9ef9dc5c154ea6adc0412b1866fe60c5373be440
CathalButler <cathalbutler@outlook.com>
2022-07-22 11:57:02 +0800

    Added backend code for this tutorial.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+} from '@apollo/client';
+
+// Create a httpLink that will connect ApolloClient instance with the GraphQL API.
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+// Instantiate ApolloClient by passing in the httpLink and new instance of an InMemoryCache.
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+// Render the root component of the react app. The app is wrapped with the higher-order component ApolloProvider that
+// gets passed the client as a prop
+ReactDOM.render(
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
